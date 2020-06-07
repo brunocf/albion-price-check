8965779de31cbaed1a0517a170140aee73d7e48b
Bruno Croys Felthes <43574558+bruno-olivia@users.noreply.github.com>
2020-06-07 15:37:48 -0300

    Initial release

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   extends: 'erb/typescript',
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
-    'import/no-extraneous-dependencies': 'off',
-    "react/prop-types": "0"
+    'import/no-extraneous-dependencies': 'off'
   },
   settings: {
     'import/resolver': {
